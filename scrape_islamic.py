399039f9fd3ca8c0552ad0aa2513ce6daa6d85b8
Ubaid kagdi <58435969+UK11-Ubaid@users.noreply.github.com>
2025-11-18 17:09:05 +0200

    Update scrape_islamic.py

--- scrape_islamic.py
+++ scrape_islamic.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import time
