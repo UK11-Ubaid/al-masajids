c08c57d529425b086e83812e14e80bb3b0a4bb9f
Ubaid kagdi <58435969+UK11-Ubaid@users.noreply.github.com>
2025-11-18 16:52:14 +0200

    Create scrape_islamic.py

---
+++ scrape_islamic.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
+import time
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://jamiat.org.za/monthly-islamic-calendar/")
+time.sleep(3)
+
+soup = BeautifulSoup(driver.page_source, "html.parser")
+driver.quit()
+
+islamic_el = soup.find("div", class_="value islamic hd-out")
+islamic_date = islamic_el.get_text(strip=True) if islamic_el else None
+
+# Load local index.html
+with open("index.html", "r", encoding="utf-8") as f:
+    html = f.read()
+
+soup = BeautifulSoup(html, "html.parser")
+
+span = soup.find("span", id="prayer-islamic-date")
+if span and islamic_date:
+    span.string = islamic_date
+
+with open("index.html", "w", encoding="utf-8") as f:
+    f.write(str(soup))
